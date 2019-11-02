626fb5a9bab7bee8223f9c1120312b208f5a3531
saahil-jain <saahiljain98@gmail.com>
2019-11-02 17:05:55 +0530

    function for filetransfer

--- TCPserver.py
+++ TCPserver.py
@@ -2,6 +2,30 @@ import os
 import daemon
 from socket import *
 
+def myrecurse(connectionSocket, filepath):
+	try:
+		onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	except:
+		pass
+
+
+def filetransfer(connectionSocket, filename):
+	try:
+		print('Opening file ', filename)
+		with open(filename, 'ab+') as fa:
+			print('Opened file')
+			fa.seek(0, 0)
+			print("Sending file.")
+			while True:
+				data = fa.read(1024)
+				connectionSocket.send(data)
+				if not data:
+					break
+			fa.close()
+			print("Sent file.")
+	except:
+		pass
+
 with daemon.DaemonContext():
 	serverPort = 12008
 	serverSocket = socket(AF_INET,SOCK_STREAM)
@@ -18,34 +42,24 @@ with daemon.DaemonContext():
 			option = 0
 		if request[0] == "get":
 			if option == 0:
-				pass
-				print('Opening file ', filename)
-				with open(filename, 'ab+') as fa:
-					print('Opened file')
-					fa.seek(0, 0)
-					print("Sending file.")
+				filetransfer(connectionSocket, filename)
+			elif option == '-r':
+				myrecurse(connectionSocket, filename)
+		elif request[0] == "post":
+			try:
+				with open(filename, "wb") as fw:
+					print("Receiving..")
 					while True:
-						data = fa.read(1024)
-						connectionSocket.send(data)
+						print('receiving')
+						data = connectionSocket.recv(1024)
+						print('Received: ', data.decode('utf-8'))
 						if not data:
+							print('Breaking from file write')
 							break
-					fa.close()
-					print("Sent file.")
-			else:
+						fw.write(data)
+						print('Wrote to file', data.decode('utf-8'))
+					fw.close()
+					print("Received..")
+			except:
 				pass
-		if request[0] == "post":
-			with open(filename, "wb") as fw:
-				print("Receiving..")
-				while True:
-					print('receiving')
-					data = connectionSocket.recv(1024)
-					print('Received: ', data.decode('utf-8'))
-					if not data:
-						print('Breaking from file write')
-						break
-					fw.write(data)
-					print('Wrote to file', data.decode('utf-8'))
-				fw.close()
-				print("Received..")
-
-		connectionSocket.close()	
+		connectionSocket.close()
