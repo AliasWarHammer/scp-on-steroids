d7c1858c37ca3ad0c3cc8d19d5ef39e93926fc8f
saahil-jain <saahiljain98@gmail.com>
2019-11-02 16:30:58 +0530

    get and post

--- TCPserver.py
+++ TCPserver.py
@@ -1,3 +1,4 @@
+import os
 from socket import *
 serverPort = 12008
 serverSocket = socket(AF_INET,SOCK_STREAM)
@@ -6,7 +7,42 @@ serverSocket.listen(1)
 print("The server is ready to receive")
 while 1:
 	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024)
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence)
+	request = connectionSocket.recv(1024)
+	if len(request.split() == 3):
+		request, filename, option = request.split()
+	else:
+		request, filename = request.split()
+		option = 0
+	if request[0] == "get":
+		if option == 0:
+			pass
+			print('Opening file ', filename)
+			with open(filename, 'ab+') as fa:
+				print('Opened file')
+				fa.seek(0, 0)
+				print("Sending file.")
+				while True:
+					data = fa.read(1024)
+					connectionSocket.send(data)
+					if not data:
+						break
+				fa.close()
+				print("Sent file.")
+		else:
+			pass
+	if request[0] == "post":
+		with open(filename, "wb") as fw:
+			print("Receiving..")
+			while True:
+				print('receiving')
+				data = connectionSocket.recv(1024)
+				print('Received: ', data.decode('utf-8'))
+				if not data:
+					print('Breaking from file write')
+					break
+				fw.write(data)
+				print('Wrote to file', data.decode('utf-8'))
+			fw.close()
+			print("Received..")
+
 	connectionSocket.close()	
